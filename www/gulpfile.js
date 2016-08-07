d405f98e8cf5816373170c5c20c9d77eefdd0f97
Jason Parent <ParentJ@advisory.com>
2016-08-07 13:16:13 -0400

    Added content.

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require("gulp");
 
 var concat = require("gulp-concat");
 var del = require("del");
-var jshint = require("gulp-jshint");
-var karma = require("karma");
+// var jshint = require("gulp-jshint");
+// var karma = require("karma");
 var rename = require("gulp-rename");
 var rimraf = require("gulp-rimraf");
-var uglify = require("gulp-uglify");
+// var uglify = require("gulp-uglify");
 
-var Server = karma.Server;
+// var Server = karma.Server;
 
 gulp.task("clean", function () {
   return del(["./dist/scripts.js"]);
@@ -16,8 +16,8 @@ gulp.task("clean", function () {
 
 gulp.task("scripts", ["clean"], function () {
   gulp.src("./app/src/**/*.js")
-    .pipe(jshint())
-    .pipe(jshint.reporter("default"))
+    //.pipe(jshint())
+    //.pipe(jshint.reporter("default"))
     //.pipe(concat("scripts.min.js"))
     //.pipe(uglify())
     .pipe(concat("scripts.js"))
